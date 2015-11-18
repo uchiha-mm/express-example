3aeecb4c530a9cb1ff4f68f5b927db5a912ea4dc
mbabbar <mbabbar@ncsu.edu>
2015-11-18 02:00:47 -0800

    featureFlag + Monitoring

--- main.py
+++ main.py
@@ -19,17 +19,74 @@ def printQueue():
 		print "Element in Queue : ", x
 		q.put(x)
 
+def makeListOfQueue(queue):
+	k = list()	
+	while(1) :
+		x = queue.get()
+		if x is None : 
+			break;
+		else :
+			k.append(x)
+	for i in k:
+		queue.put(i)
+	return k
 
 def main(argv) :
 	global q
+
+	#					
+	# python main.py 	/a 		ON 		3000
+	# python main.py 	/a 		OFF 	3000
+	# python main.py 	/a 		RM 		featureFlag
+	# python main.py 	3000 	RM 		server
 
 	featureName = argv[1]
 	featureStatus = argv[2]
 	featureServer = argv[3]
 
 	q = HotQueue(('server_'+featureName), host="localhost", port=6379, db=0)
+	featureNames = HotQueue('featureNames', host="localhost", port=6379, db=0)
+
+	r_server = redis.Redis('localhost')
+
+	if featureStatus == "RM":
+		if featureServer == "featureFlag":
+			r_server.delete(featureName);
+			while(1) :
+				x = q.get()
+				if x is None : 
+					return;
+
+
+		if featureServer == "server":
+			listOfFeatures = makeListOfQueue(featureNames) 
+			for i in listOfFeatures:	
+				q = HotQueue((i), host="localhost", port=6379, db=0)
+				k = list()	
+				while(1) :
+					x = q.get()
+					if x is None : 
+						break;
+					else :
+						if x == ('http://127.0.0.1:'+featureName) :
+							if(r_server.get(i[7:]) > 0):
+								r_server.decr(i[7:])
+							break
+						else:
+							k.append(x)
+				while len(k) != 0:
+					x = k.pop(0)
+					q.put(x)
+
+	
 	if featureStatus == "ON":
 		q.put('http://127.0.0.1:'+featureServer)
+		if(not r_server.exists(featureName)):
+			r_server.set(featureName, '1');
+			featureNames.put('server_'+featureName);
+		else:
+			r_server.incr(featureName)
+		print 'set key value is: ' + r_server.get(featureName) 
 	else :
 		k = list()	
 		while(1) :
@@ -40,6 +97,9 @@ def main(argv) :
 			else :
 				if x == ('http://127.0.0.1:'+featureServer) :
 					print("Removing : ",x)
+					if(r_server.get(featureName) > 0):
+						r_server.decr(featureName)
+					print 'set key value is: ' + r_server.get(featureName) 
 					break
 				else:
 					k.append(x)
@@ -47,6 +107,7 @@ def main(argv) :
 			x = k.pop(0)
 			q.put(x)
 	printQueue()
+	print "Queue name : ",('server_'+featureName)
 	
 print(sys.argv)
 main(sys.argv)
