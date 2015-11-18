ca6d5fc4dba53ce9c7d773d9b7d5004ecd494d1a
mbabbar <mbabbar@ncsu.edu>
2015-11-18 14:51:59 -0800

    Final Test

--- health.sh
+++ health.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
 CONTAINER=$1
+devPort="300"
 
 /usr/bin/docker stats --no-stream $CONTAINER > stat
 
 container_cpu=`cat stat | grep $CONTAINER | tr -s [:space:] " " | cut -d" " -f2 | sed -e 's/%//g'`
-#container_cpu=$((container_cpu*100))
-comp_cpu="0.00"
+container_port=`sudo /usr/bin/docker ps | grep $CONTAINER | tr -s [:space:] " " | cut -d" " -f10 | cut -d":" -f2 | cut -d"-" -f1`
+comp_cpu="20.00"
 if ! echo "$container_cpu $comp_cpu -p" | dc | grep > /dev/null ^-; then
-#if [ $container_cpu > 0.00 ]; then
-	echo "Its above zero"
-	echo $container_cpu
-	node sendmail.js $CONTAINER CPU $container_cpu
+        echo "Its above the threshold"
+        echo $container_cpu
+        echo $container_port
+        if [[ "$container_port" == "$devPort"* ]]; then
+                python main.py $container_port RM server
+        fi
+        node sendmail.js $CONTAINER CPU $container_cpu
 fi
 
 
-comp_mem="0.00"
+comp_mem="6.00"
 container_mem=`cat stat | grep $CONTAINER | tr -s [:space:] " " | cut -d" " -f8 | sed -e 's/%//g'`
-#if [ $container_mem > 0.00 ]; then
-if ! echo "$container_mem $comp_mem -p" | dc | grep > /dev/null ^-; then 
-       	echo "Its above zero"
+if ! echo "$container_mem $comp_mem -p" | dc | grep > /dev/null ^-; then
+        echo "Its above the threshold"
         echo $container_mem
-	node sendmail.js $CONTAINER MEMORY $container_mem
+        echo $container_port
+        if [[ "$container_port" == "$devPort"* ]]; then
+                python main.py $container_port RM server
+        fi
+        node sendmail.js $CONTAINER MEMORY $container_mem
 fi
--- proxy.js
+++ proxy.js
@@ -10,64 +10,47 @@ var app = express()
 var client = redis.createClient(6379, '127.0.0.1', {})
 
 
+
 /**************Running Proxy on port 9000****************/
 /**************Running Main.js on port 3000 and 3001****************/
-client.del('servers');
-client.lpush(['servers', 'http://127.0.0.1:3000'], function(err, reply){
-	console.log(reply);
-})
-/*client.lpush(['servers', 'http://127.0.0.1:3001'], function(err, reply){
-	console.log(reply);
-})
-client.lpush(['servers', 'http://127.0.0.1:4000'], function(err, reply){
-	console.log(reply);
-})*/
+
 
 var options = {};
 var proxy = httpProxy.createProxyServer(options);
 
 var server = http.createServer(function(req, res){
 	console.log(req.method, req.url);
-	//client.get(("server_"+req.url), function(err,value){ console.log(value)});
-	// client.rpoplpush('server_'+req.url, function(err, value){
-	// 	console.log('Zeeshan ki value :',value);
-	// })
+	
 	client.get(req.url, function(err, value){
-	if(value)
-		console.log('The queue has elements :', value)
-		if(value == 0){
-			console.log('No server to serve this feature');
-			proxy.web(req, res, {target: 'http://127.0.0.1:3000'});
+		if(value){
+			console.log('The queue has elements :', value)
+			if(value == 0){
+				console.log('No server to serve this feature');
+				proxy.web(req, res, {target: 'http://127.0.0.1:3000'});
+			}
+			else if (value>0){
+				  	client.rpop('hotqueue:server_'+req.url, function(err,value){
+				  	console.log("Popped server name is : ", value); 
+				  	var ip = value.slice(2, 23)
+				  	console.log('ip now is :', ip)
+				  	proxy.web(req, res, {target: ip});
+				  	client.lpush(['hotqueue:server_'+req.url, value], function(err, reply){	
+				  		console.log(reply);	
+				  	})  
+				  })
+			}
 		}
-		else if (value>0){
-			  	client.rpop('hotqueue:server_'+req.url, function(err,value){
+		else{
+			client.rpop('servers', function(err,value){
 			  	console.log("Popped server name is : ", value); 
-			  	var ip = value.slice(2, 23)
-			  	console.log('ip now is :', ip)
-			  	proxy.web(req, res, {target: ip});
-			  	client.lpush(['hotqueue:server_'+req.url, value], function(err, reply){	
+			  	proxy.web(req, res, {target: value});
+			  	client.lpush(['servers', value], function(err, reply){	
 			  		console.log(reply);	
 			  	})  
-			  })
+			 })
 		}
-	else{
-		client.rpop('servers', function(err,value){
-		  	console.log("Popped server name is : ", value); 
-		  	proxy.web(req, res, {target: value});
-		  	client.lpush(['servers', value], function(err, reply){	
-		  		console.log(reply);	
-		  	})  
-		 })
-	}
 		
 	});
 
-	/*client.rpoplpush('hotqueue:server_'+req.url, 'server_/a', function(err,value){
-		console.log("Nahi chal raha : ", value)
-	})
-	client.rpoplpush('servers', 'servers', function(err,value){
-		proxy.web(req, res, {target: value});
-			console.log("Server is running on port : ", value)
-	})*/
 });
 server.listen (9000);
